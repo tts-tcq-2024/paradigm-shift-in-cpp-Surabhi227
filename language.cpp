f0419ef18a73db69315ee92bcf23d7708bbb62f3
Surabhi227 <surabhiravishankar@gmail.com>
2024-08-01 16:58:49 +0530

    Update language.cpp

--- language.cpp
+++ language.cpp
@@ -1,28 +1,25 @@
 #include "language.h"
-#include <map>
-#include <string>
 
-using namespace std;
 
-Language currentLanguage = ENGLISH;
-
-map<string, map<Language, string>> errorMessages;
-
-void InitializeErrorMessages() {
-  errorMessages["Temperature out of range!"] = {
-    {ENGLISH, "Temperature out of range!"},
-    {GERMAN, "Temperatur außerhalb des zulässigen Bereichs!"}
-  };
-  errorMessages["State of Charge out of range!"] = {
-    {ENGLISH, "State of Charge out of range!"},
-    {GERMAN, "Ladezustand außerhalb des zulässigen Bereichs!"}
-  };
-  errorMessages["Charge Rate out of range!"] = {
-    {ENGLISH, "Charge Rate out of range!"},
-    {GERMAN, "Laderate außerhalb des zulässigen Bereichs!"}
-  };
-}
-
-const char* GetErrorMessage(const char* messageKey) {
-  return errorMessages[messageKey][currentLanguage].c_str();
-}
+std::map<Language, std::map<std::string, std::string>> messages = {
+    {ENGLISH, {
+        {"TEMP_LOW_WARNING", "Warning: Approaching low temperature"},
+        {"TEMP_HIGH_WARNING", "Warning: Approaching high temperature"},
+        {"TEMP_BREACH", "Temperature out of range!"},
+        {"SOC_LOW_WARNING", "Warning: Approaching discharge"},
+        {"SOC_HIGH_WARNING", "Warning: Approaching charge-peak"},
+        {"SOC_BREACH", "State of Charge out of range!"},
+        {"CHARGE_RATE_WARNING", "Warning: Approaching high charge rate"},
+        {"CHARGE_RATE_BREACH", "Charge Rate out of range!"}
+    }},
+    {GERMAN, {
+        {"TEMP_LOW_WARNING", "Warnung: Näher an niedriger Temperatur"},
+        {"TEMP_HIGH_WARNING", "Warnung: Näher an hoher Temperatur"},
+        {"TEMP_BREACH", "Temperatur außerhalb des Bereichs!"},
+        {"SOC_LOW_WARNING", "Warnung: Näher an Entladung"},
+        {"SOC_HIGH_WARNING", "Warnung: Näher an Ladepeak"},
+        {"SOC_BREACH", "Ladezustand außerhalb des Bereichs!"},
+        {"CHARGE_RATE_WARNING", "Warnung: Näher an hoher Ladegeschwindigkeit"},
+        {"CHARGE_RATE_BREACH", "Ladegeschwindigkeit außerhalb des Bereichs!"}
+    }}
+};
