7c08cc911f5c9bcd21d438b58ed342f2c0b9fa65
Surabhi227 <surabhiravishankar@gmail.com>
2024-08-01 17:16:02 +0530

    Update BatteryMonitor.cpp

--- BatteryMonitor.cpp
+++ BatteryMonitor.cpp
@@ -1,52 +1,29 @@
 #include "BatteryMonitor.h"
+#include "Messages.h"
 #include <iostream>
 
-// Constants definitions
-const float TEMP_MIN = 0;
-const float TEMP_MAX = 45;
-const float SOC_MIN = 20;
-const float SOC_MAX = 80;
-const float CHARGE_RATE_MIN = 0;
-const float CHARGE_RATE_MAX = 0.8;
-const float WARNING_TOLERANCE = 0.05;
-
-// Message maps and current language
-std::map<std::string, std::string> messages;
-Language currentLanguage = ENGLISH;
-
-// Helper functions
-bool IsOutOfRange(float value, float min, float max) {
-    return value < min || value > max;
-}
-
-bool PrintBreachMessage(float value, float min, float max, const std::string& breachMsg) {
-    if (IsOutOfRange(value, min, max)) {
-        std::cout << messages[breachMsg] << std::endl;
+bool CheckInRange(float value, float min, float max, float tolerance,
+                  const std::string& breachMsg, const std::string& lowWarningMsg,
+                  const std::string& highWarningMsg) {
+    if (value < min || value > max) {
+        std::cout << messages[currentLanguage][breachMsg] << std::endl;
         return false;
     }
-    return true;
-}
-
-bool PrintWarningMessages(float value, float min, float max, float tolerance, 
-                          const std::string& lowWarningMsg, const std::string& highWarningMsg) {
     if (value < min + tolerance) {
-        std::cout << messages[lowWarningMsg] << std::endl;
+        std::cout << messages[currentLanguage][lowWarningMsg] << std::endl;
     }
     if (value > max - tolerance) {
-        std::cout << messages[highWarningMsg] << std::endl;
+        std::cout << messages[currentLanguage][highWarningMsg] << std::endl;
     }
     return true;
 }
 
 bool BatteryIsOk(float temperature, float soc, float chargeRate) {
-    bool temperatureOk = PrintBreachMessage(temperature, TEMP_MIN, TEMP_MAX, "TEMP_BREACH") &&
-                         PrintWarningMessages(temperature, TEMP_MIN, TEMP_MAX, TEMP_MAX * WARNING_TOLERANCE, 
-                                              "TEMP_LOW_WARNING", "TEMP_HIGH_WARNING");
-    bool socOk = PrintBreachMessage(soc, SOC_MIN, SOC_MAX, "SOC_BREACH") &&
-                 PrintWarningMessages(soc, SOC_MIN, SOC_MAX, SOC_MAX * WARNING_TOLERANCE, 
-                                      "SOC_LOW_WARNING", "SOC_HIGH_WARNING");
-    bool chargeRateOk = PrintBreachMessage(chargeRate, CHARGE_RATE_MIN, CHARGE_RATE_MAX, "CHARGE_RATE_BREACH") &&
-                        PrintWarningMessages(chargeRate, CHARGE_RATE_MIN, CHARGE_RATE_MAX, CHARGE_RATE_MAX * WARNING_TOLERANCE, 
-                                             "CHARGE_RATE_WARNING", "CHARGE_RATE_WARNING");
+    bool temperatureOk = CheckInRange(temperature, TEMP_MIN, TEMP_MAX, TEMP_MAX * WARNING_TOLERANCE, 
+                                      "TEMP_BREACH", "TEMP_LOW_WARNING", "TEMP_HIGH_WARNING");
+    bool socOk = CheckInRange(soc, SOC_MIN, SOC_MAX, SOC_MAX * WARNING_TOLERANCE, 
+                              "SOC_BREACH", "SOC_LOW_WARNING", "SOC_HIGH_WARNING");
+    bool chargeRateOk = CheckInRange(chargeRate, CHARGE_RATE_MIN, CHARGE_RATE_MAX, CHARGE_RATE_MAX * WARNING_TOLERANCE, 
+                                     "CHARGE_RATE_BREACH", "CHARGE_RATE_WARNING", "CHARGE_RATE_WARNING");
     return temperatureOk && socOk && chargeRateOk;
 }
